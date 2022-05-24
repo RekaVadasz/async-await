c0b3c5e176630a8a72abc4cf4e933101c944f3a8
RekaVadasz <vadasz.reka87@gmail.com>
2022-05-24 18:31:30 +0200

    added some comments

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,27 +1,31 @@
 
 
-const loadEvent = async function (){ //egész függvény aszinkron módon tud futni
+const loadEvent = async function (){ //egész függvény aszinkron módon tud futni, ha az "async" szót elé írjuk
     const rootElement = document.getElementById("root");
 
-    /* fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY") //ez akkor, ha NEM async a loadEvent függvény
-    .then(function(response){
-        console.log(response);
-        return response.json(); //json metódus
+    fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY") //a .then-es megoldás  akkor, ha NEM async a loadEvent függvény
+    /*
+    .then(function(response){  //.then method: egy functiont tartalmaz aminek a paramétere a response. The response parameter takes the value of the object returned from fetch(url).
+        console.log(response); // response: object with a series of methods that can be used depending on what you want to do with the information 
+        return response.json(); //json metódus: a response objektumot JSON-ná alakítja
+
     })
     .then(function(json){
-        console.log(json.date);
+        console.log(json) //kiírja magát a jsont, amit az előző then-ben létrehoztunk 
+        console.log(json.date); // kiírja a json objektum "date" kulcsának értékét
         rootElement.insertAdjacentHTML("beforeend", `
             <img src="${json.hdurl}">
         `)
-    }) */
+    }) 
+    */
 
-    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"); //ugyanezt csinálja, mint a fenti
-
+    //ugyanezt csinálja, mint a fenti, csak async/await használatával:
+    const response = await fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"); 
     const responseJson = await response.json();
     rootElement.insertAdjacentHTML("beforeend", `
-            <img src="${responseJson.hdurl}">
-        `)
-
+    <img src="${responseJson.hdurl}">
+    `)
+    console.log(responseJson) //ennek az objektumnak a hdurl kulcsának értékére van szükségünk (kép url címe) 
 }
 
 window.addEventListener("load", loadEvent);
